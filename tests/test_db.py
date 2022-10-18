0563620c68b692fdc6745d61c4ea4d2fb416a4e1
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-10-18 17:20:55 -0400

    Added GET tests for DB

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,6 +4,7 @@ import unittest
 from peewee import *
 
 from app import TimelinePost
+from playhouse.shortcuts import model_to_dict
 
 MODELS = [TimelinePost]
 
@@ -11,7 +12,7 @@ MODELS = [TimelinePost]
 test_db = SqliteDatabase(':memory:')
 
 class TestTimelinePost(unittest.TestCase):
-    def detUp(self):
+    def setUp(self):
         #Bind model classes to test db. Since we have a complete
         #list of all models, we do not need to recursively bind dependencies.
         test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
@@ -30,10 +31,20 @@ class TestTimelinePost(unittest.TestCase):
 
     def test_timeline_post(self):
         #create 2 timeline posts
-        first_post = TimelinePost.create(name='John Doe', 
-email='john@example.com', content='Hello world, I\'m John!')
+        first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
         assert first_post.id == 1
-        second_post = TimelinePost.create(name='Jane Doe', 
-email='jane@example.com', content='Hello world, I\'m Jane!')
+        second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
         assert second_post.id == 2
-        # TODO: Get timeline posts and assert that they are correct
+        
+        #Get timeline posts and assert that they are correct by name
+        get_timeline_posts =[
+            model_to_dict(p)
+            for p in TimelinePost
+            ]
+
+        assert get_timeline_posts[0]['name'] == 'John Doe'
+        assert get_timeline_posts[1]['name'] == 'Jane Doe'
+        assert get_timeline_posts[0]['email'] == 'john@example.com'
+        assert get_timeline_posts[1]['email'] == 'jane@example.com'
+        assert get_timeline_posts[0]['content'] == 'Hello world, I\'m John!'
+        assert get_timeline_posts[1]['content'] == 'Hello world, I\'m Jane!'
